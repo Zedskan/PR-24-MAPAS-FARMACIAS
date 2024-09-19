8e61696225c485dbb0c71b3dd658c1a55d252bed
Cristhian Escalera <141873863+CristhianEscalera@users.noreply.github.com>
2024-09-19 00:17:47 -0400

    Add files via upload

---
+++ MAPAS_FARMACIAS/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MAPAS_FARMACIAS
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
